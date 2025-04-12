6f4fe07b208c675fc1670a1a943e4c588bb735e6
Israel Effiong <israel.iszy2336@gmail.com>
2025-04-12 17:12:58 +0100

    Update waitlist-form.tsx

--- components/waitlist-form.tsx
+++ components/waitlist-form.tsx
@@ -311,7 +311,7 @@ export default function WaitlistForm() {
                         </Label>
                         <p className="font-risque text-sm text-gray-400 dark:text-gray-400">
                           <a
-                            href="https://x.com/LeipPass/status/1911028384039129485"
+                            href="https://x.com/LeipPass/status/1911089062254948857"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-400 hover:underline dark:text-blue-400"
